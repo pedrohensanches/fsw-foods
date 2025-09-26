86eed2fbb96c3d9e246da58307af68776e158486
Pedro Henrique Sanches da Silva <pedrohensanches@outlook.com>
2025-09-26 17:02:16 -0300

    feat: add initial header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
+import Header from "./_components/header";
+
 const Home = () => {
   return (
-    <div className="bg-red-500 px-4 py-2 mx-6">
-      <h1>Hello tailwind!!!</h1>
-    </div>
+    <Header/>
   );
 };
 
