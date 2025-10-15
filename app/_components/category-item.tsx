96f0bba8332df475fb9893b89fe03a37acb742d7
Pedro Henrique Sanches da Silva <pedrohensanches@outlook.com>
2025-10-14 23:13:56 -0300

    style: update category components styles

--- app/_components/category-item.tsx
+++ app/_components/category-item.tsx
@@ -7,7 +7,7 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
-    <div className="flex items-center gap-3 py-3 px-4 bg-white shadow-md rounded-full">
+    <div className="flex items-center justify-center gap-3 rounded-full bg-white px-4 py-3 shadow-md">
       <Image
         src={category.imageUrl}
         alt={category.name}
